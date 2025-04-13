ecf1e481dfc372980351bb26407cee131d40d840
AllanM8 <allanmonis8@gmail.com>
2025-04-13 17:07:33 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages & Components
 import Home from "./components/Home";
 import Properties from "./components/Properties";
 import SingleProperty from "./components/SingleProperty";
@@ -9,19 +11,26 @@ import Login from "./components/Login";
 import Signup from "./components/Signup"; 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import UserDashboard from "./components/Dashboard/UserDashboard";
+import Tenant from "./components/Dashboard/Tenant";
 
 function App() {
   return (
     <Router>
       <Header />
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/properties" element={<Properties />} />
-        <Route path="/properties/:id" element={<SingleProperty />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />  
-        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/" element={<Home />} />                       {/* Home Page */}
+        <Route path="/properties" element={<Properties />} />       {/* All Properties */}
+        <Route path="/properties/:id" element={<SingleProperty />} /> {/* Single Property (Old) */}
+        
+        <Route path="/contact" element={<Contact />} />             {/* Contact Page */}
+        <Route path="/login" element={<Login />} />                 {/* Login Page */}
+        <Route path="/signup" element={<Signup />} />               {/* Signup Page */}
+        <Route path="/dashboard" element={<UserDashboard />} />     {/* User Dashboard */}
+        <Route path="/rental" element={<Tenant />} />               {/* Rental Graph / Data */}
       </Routes>
+
       <Footer />
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,28 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link className="navbar-brand" to="/">EstateBit</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/properties">Properties</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/services">Services</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/signup">
-              <button className="btn btn-primary">Sign Up</button>
-              </Link>
-            </li>
-          </ul>
+    <nav className="bg-white shadow-md fixed w-full top-0 z-50">
+      <div className="container mx-auto flex justify-between items-center py-1 px-2">
+        {/* Logo */}
+        <Link className="text-2xl font-bold text-blue-600" to="/">
+          B<span className="text-yellow-500">Loc</span>Realty.
+        </Link>
+
+        {/* Navigation Links */}
+        <div className="hidden md:flex space-x-6">
+          <Link className="text-gray-700 hover:text-blue-600" to="/">Home</Link>
+          <Link className="text-gray-700 hover:text-blue-600" to="/properties">Properties</Link>
+          <Link className="text-gray-700 hover:text-blue-600" to="/services">Services</Link>
+          <Link className="text-gray-700 hover:text-blue-600" to="/about">About</Link>
+          <Link className="text-gray-700 hover:text-blue-600" to="/contact">Contact</Link>
         </div>
+
+        {/* Sign Up Button */}
+        <Link to="/signup">
+          <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </nav>
   );
